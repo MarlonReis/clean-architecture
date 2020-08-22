a42fc532359adb288523920da0e7ec9773d4d506
MarlonReis <marlon-reis91@hotmail.com>
2020-08-22 16:46:36 -0300

    test: ensure EmailValidatorAdapter returns true when validator return true

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -1,8 +1,23 @@
 import { EmailValidatorAdapter } from './email-validator-adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+    isEmail (): boolean {
+        return true
+    }
+}))
+
 describe('EmailValidator adapter', () => {
     test('Should be return false when validator return false', () => {
         const sut = new EmailValidatorAdapter()
+        jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
         const isValid = sut.isValid('invalid@email.com.br')
         expect(isValid).toBe(false)
+    })
+
+    test('Should be return true when validator return true', () => {
+        const sut = new EmailValidatorAdapter()
+        const isValid = sut.isValid('valid@email.com.br')
+        expect(isValid).toBe(true)
     })
 })
