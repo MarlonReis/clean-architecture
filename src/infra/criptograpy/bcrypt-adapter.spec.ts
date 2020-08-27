42e00b15cbc5757b295fa91744ef1927a69cc377
Marlon Reis <marlon-reis91@hotmail.com>
2020-08-27 19:32:17 -0300

    test: ensore throws when bcrypt throws error

--- src/infra/criptograpy/bcrypt-adapter.spec.ts
+++ src/infra/criptograpy/bcrypt-adapter.spec.ts
@@ -9,21 +9,28 @@ jest.mock('bcrypt', () => ({
 }))
 
 const salt = 12
-const sutBCryptAdapterFactory = (): BCryptAdapter => {
+const makeSutBCryptAdapterFactory = (): BCryptAdapter => {
     return new BCryptAdapter(salt)
 }
 
 describe('Bcrypt adapter ', () => {
     test('Should be call bcrypt with correct values', async () => {
-        const sut = sutBCryptAdapterFactory()
+        const sut = makeSutBCryptAdapterFactory()
         const hashSpy = jest.spyOn(bcrypt, 'hash')
         await sut.encrypt('anyValue')
         expect(hashSpy).toHaveBeenCalledWith('anyValue', salt)
     })
 
     test('Should be return a hash on success', async () => {
-        const sut = sutBCryptAdapterFactory()
+        const sut = makeSutBCryptAdapterFactory()
         const hash = await sut.encrypt('anyValue')
         expect(hash).toBe('hash')
+    })
+
+    test('Should be throws when bcrypt throws error', async () => {
+        const sut = makeSutBCryptAdapterFactory()
+        jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(Promise.reject(new Error()))
+        const hashPromise = sut.encrypt('anyValue')
+        await expect(hashPromise).rejects.toThrow()
     })
 })
