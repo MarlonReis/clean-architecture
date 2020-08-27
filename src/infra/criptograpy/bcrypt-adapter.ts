ea336378701b8ff64f26b042755036bbf48350f6
Marlon Reis <marlon-reis91@hotmail.com>
2020-08-27 13:27:44 -0300

    feat: ensure BcryptAdapter calls bcrypt with correct value

---
+++ src/infra/criptograpy/bcrypt-adapter.ts
@@ -0,0 +1,15 @@
+import bcrypt from 'bcrypt'
+
+import { Encryption } from '../../data/protocols/encryption'
+
+export class BCryptAdapter implements Encryption {
+    private readonly salt: number
+
+    constructor (salt: number) {
+        this.salt = salt
+    }
+
+    async encrypt (data: string): Promise<string> {
+        return bcrypt.hash(data, this.salt)
+    }
+}
