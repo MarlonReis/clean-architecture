e4baa532f82d9f9b18469e7e713b3644fd616d92
Marlon Reis <marlon-reis91@hotmail.com>
2020-08-27 13:30:00 -0300

    refactor: change description test

--- src/infra/criptograpy/bcrypt-adapter.spec.ts
+++ src/infra/criptograpy/bcrypt-adapter.spec.ts
@@ -3,7 +3,7 @@ import bcrypt from 'bcrypt'
 import { BCryptAdapter } from './bcrypt-adapter'
 
 describe('Bcrypt adapter ', () => {
-    test('Should be call bcrypt with correct value', async () => {
+    test('Should be call bcrypt with correct values', async () => {
         const salt = 12
         const sut = new BCryptAdapter(salt)
         const hashSpy = jest.spyOn(bcrypt, 'hash')
