9bfc834f1bd8b54e1378efb1972d772d3ead1e4a
Marlon Reis <marlon-reis91@hotmail.com>
2020-08-28 17:21:13 -0300

    refactor: move mongodb to a helper

---
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb'
+
+export const MongoHelper = {
+    mongoClient: undefined as MongoClient,
+    async connect (url: string): Promise<void> {
+        this.mongoClient = await MongoClient.connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+    },
+    async disconnect (): Promise<void> {
+       await this.mongoClient.close()
+    }
+}
