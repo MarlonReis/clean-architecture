5a89a9b0e064193cae70d082f95a38b51b60fa9e
MarlonReis <marlon-reis91@hotmail.com>
2020-08-31 11:45:08 -0300

    feat: ensure that repository insert a new register

--- src/infra/db/mongodb/account-repository/account-mapper.ts
+++
@@ -1,6 +0,0 @@
-import { AccountModel } from '../../../../domain/models/account'
-
-export const map = (account: any): AccountModel => {
-    const { _id, ...accountWithoutId } = account
-    return { id: _id, ...accountWithoutId }
-}
--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -2,12 +2,11 @@ import { AddAccountRepository } from '../../../../data/protocols/add-account-rep
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecase/add-account'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { map } from './account-mapper'
 
 export class AccountMongoRepository implements AddAccountRepository {
     async add (accountData: AddAccountModel): Promise<AccountModel> {
         const accountCollection = MongoHelper.getCollection('accounts')
         const result = await accountCollection.insertOne(accountData)
-        return map(result.ops[0])
+        return MongoHelper.map(result.ops[0])
     }
 }
